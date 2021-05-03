cfbd3fde8bdb71ca8f8b14aa6b73c9b4ebb2ae02
prcnet <brian@prcnetworks.com>
2021-05-04 11:55:12 +1200

    Push to include @Taviana in Batch
     update to Batch File Launcher

--- dayz_game.bat
+++ dayz_game.bat
@@ -1,3 +1,15 @@
-arma2oa.exe -mod=@Dayz -beta=Expansion\beta;Expansion\beta\Expansion -nosplash
+@echo off
+COLOR 0B
+set mod=%1
 
+echo DayZ Mod launcher.
+echo.
+echo 1  - DayZ Mod 1.7.4.4
+echo 2  - DayZTaviana.com 2.0
+echo.
+set /P mod=Mod number: 
+if "%mod%"=="1" start arma2oa.exe -mod=@Dayz -beta=Expansion\beta;Expansion\beta\Expansion -nosplash
+if "%mod%"=="2" start arma2oa.exe -mod=@Taviana -beta=Expansion\beta;Expansion\beta\Expansion -nosplash
+
+exit
 
